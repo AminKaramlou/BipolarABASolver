3c569cc3a25af3fc593fb14c5d48e7209fe3eef7
mak514 <mak514@ic.ac.uk>
2018-04-07 10:30:33 +0100

    Fix problems in mapping function

--- src/mappings.py
+++ src/mappings.py
@@ -13,9 +13,10 @@ def map_baf_to_naba_framework(baf_framework):
     language = assumptions.union(non_assumptions)
 
     for attack in baf_framework.attacks:
-        rules.add(Rule({attack[0]}, attack[1] + '_contrary'))
+        rules.add(Rule({Assumption(attack[0], attack[0] + '_contrary')}, Sentence(attack[1] + '_contrary')))
     for support in baf_framework.supports:
-        rules.add(Rule({support[1]}, support[0]))
+        rules.add(Rule({Assumption(support[1], support[1] + '_contrary')},
+                       Assumption(support[0], support[0] + '_contrary')))
 
     return BipolarABA(language, rules, assumptions)
 
@@ -31,9 +32,10 @@ def map_baf_to_daba_framework(baf_framework):
     language = assumptions.union(non_assumptions)
 
     for attack in baf_framework.attacks:
-        rules.add(Rule({attack[0]}, attack[1] + '_contrary'))
+        rules.add(Rule({Assumption(attack[0], attack[0] + '_contrary')}, Sentence(attack[1] + '_contrary')))
     for support in baf_framework.supports:
-        rules.add(Rule({support[0]}, support[1]))
+        rules.add(Rule({Assumption(support[0], support[0] + '_contrary')},
+                       Assumption(support[1], support[1] + '_contrary')))
 
     return BipolarABA(language, rules, assumptions)
 
