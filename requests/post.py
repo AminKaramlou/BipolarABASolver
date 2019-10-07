374138035f9ff5d76d7b924073ec8272742c446c
kcyras <k.cyras@imperial.ac.uk>
2019-10-07 16:14:20 +0100

    small script for POSTing DSS input and getting arg output

---
+++ requests/post.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+
+import json
+import os.path
+import requests
+from requests.exceptions import HTTPError
+
+URL = "https://aba-plus-g.herokuapp.com/generate_explanations"
+# URL = "http://127.0.0.1:5000/"
+INPUT_FILE = "DSS2Arg_useCase(NOPref).json"
+
+with open(INPUT_FILE, "rb") as DSS_input_file:
+	DSS_input = json.load(DSS_input_file)
+
+try:
+	response = requests.post(URL, json = DSS_input)
+	if response:
+		arg_output = response.json()
+		OUTPUT_FILE = f"{os.path.join(os.path.splitext(INPUT_FILE)[0] + '_arg', os.path.splitext(INPUT_FILE)[1])}"
+		with open(OUTPUT_FILE, "w") as write_file:
+			json.dump(arg_output, write_file)
+		print(f"Argumentation output written in {OUTPUT_FILE}.")
+
+	response.raise_for_status()
+except HTTPError as http_err:
+	print(f'HTTP error occurred: {http_err}')
+except Exception as err:
+	print(f'Other error occurred: {err}')
+else:
+	print(response.status_code, response.reason)
+
+
+
+
+
+
+
+
